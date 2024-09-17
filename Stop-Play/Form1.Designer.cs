5a1fb1f4b085371fe609ad6a3eff071bd7cf73a6
Dmitriy Dmitriy <dbilica19@gmail.com>
2024-09-17 19:44:55 +0300

    Add forms and click button

--- Stop-Play/Form1.Designer.cs
+++ Stop-Play/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnStop = new Button();
+            btnPause = new Button();
+            btnStart = new Button();
+            lblNumber = new Label();
+            SuspendLayout();
+            // 
+            // btnStop
+            // 
+            btnStop.BackColor = Color.IndianRed;
+            btnStop.Location = new Point(542, 197);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(126, 57);
+            btnStop.TabIndex = 0;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = false;
+            btnStop.Click += btnStop_Click_1;
+            // 
+            // btnPause
+            // 
+            btnPause.BackColor = SystemColors.ActiveCaption;
+            btnPause.Location = new Point(337, 197);
+            btnPause.Name = "btnPause";
+            btnPause.Size = new Size(126, 57);
+            btnPause.TabIndex = 1;
+            btnPause.Text = "Pause";
+            btnPause.UseVisualStyleBackColor = false;
+            btnPause.Click += btnPause_Click_1;
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.LawnGreen;
+            btnStart.Location = new Point(134, 197);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(126, 57);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += btnStart_Click_1;
+            // 
+            // lblNumber
+            // 
+            lblNumber.AutoSize = true;
+            lblNumber.BackColor = Color.Gold;
+            lblNumber.BorderStyle = BorderStyle.FixedSingle;
+            lblNumber.Location = new Point(354, 93);
+            lblNumber.Name = "lblNumber";
+            lblNumber.Size = new Size(96, 17);
+            lblNumber.TabIndex = 3;
+            lblNumber.Text = "вывод счетчика";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblNumber);
+            Controls.Add(btnStart);
+            Controls.Add(btnPause);
+            Controls.Add(btnStop);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnStop;
+        private Button btnPause;
+        private Button btnStart;
+        private Label lblNumber;
     }
 }
--- Stop-Play/Form1.cs
+++ Stop-Play/Form1.cs
@@ -2,9 +2,77 @@ namespace Stop_Play
 {
     public partial class Form1 : Form
     {
+        private Thread incrementThread; // Поток для инкремента
+        private int number = 0; // Число для инкрементации
+        private bool isRunning = false; // Флаг для проверки работы потока
+        private ManualResetEvent pauseEvent = new ManualResetEvent(true); // Событие для паузы и возобновления
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            if (incrementThread == null || !incrementThread.IsAlive)
+            {
+                isRunning = true;
+                incrementThread = new Thread(IncrementNumber);
+                incrementThread.IsBackground = true;
+                incrementThread.Start();
+            }
+            pauseEvent.Set();
+        }
+
+        private void btnPause_Click_1(object sender, EventArgs e)
+        {
+            if (pauseEvent.WaitOne(0))
+            {
+                pauseEvent.Reset();
+            }
+            else
+            {
+                pauseEvent.Set();
+            }
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            isRunning = false;
+            pauseEvent.Set();
+            if (incrementThread != null && incrementThread.IsAlive)
+            {
+                incrementThread.Join();
+            }
+        }
+
+        private void IncrementNumber()
+        {
+            while (isRunning)
+            {
+                pauseEvent.WaitOne();
+
+                number++;
+                if (number % 10 == 0)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        lblNumber.Text = number.ToString();
+                    }));
+                }
+
+                Thread.Sleep(200);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isRunning = false;
+            pauseEvent.Set();
+            if (incrementThread != null && incrementThread.IsAlive)
+            {
+                incrementThread.Join();
+            }
         }
     }
 }
